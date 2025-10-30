d8b3952e80c813ba7b6784600201004704fb66be
johnholloway77 <jghollow@ucalgary.ca>
2025-10-30 16:32:35 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,148 @@
+#define _POSIX_C_SOURCE 200809L
+#define __BSD_VISIBLE 1
+
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define SH_TOK_BUFSIZE 64
+#define SH_TOK_DELIM " \t\r\n\a"
+
+#include <time.h>
+
+int sh_execute(char** args);
+int sh_launch(char** args);
+void sh_loop(void);
+char* sh_read_line(void);
+char** sh_split_line(char* line);
+
+int main(int argc, char** argv) {
+  sh_loop();
+  return EXIT_SUCCESS;
+}
+
+void sh_loop(void) {
+  char* line;
+  char** args;
+  int status;
+
+  do {
+    printf("JHsh$: ");
+    line = sh_read_line();
+    args = sh_split_line(line);
+    status = sh_execute(args);
+
+    free(line);
+    free(args);  // free's indicate we will return a pointer
+  } while (status);
+}
+
+char* sh_read_line(void) {
+  char* line = NULL;
+  size_t buffsize = 0;
+
+  if (getline(&line, &buffsize, stdin) == -1) {
+    if (feof((stdin))) {
+      fprintf(stderr, "EOF\n");
+      exit(EXIT_SUCCESS);
+    } else {
+      fprintf(stderr, "Value of errno: %d\n", errno);
+      exit(EXIT_FAILURE);
+    }
+  }
+  return line;
+}
+
+char** sh_split_line(char* line) {
+  int bufsize = SH_TOK_BUFSIZE;
+  int position = 0;
+  char *save = NULL;
+  char** tokens = malloc(bufsize * sizeof(char*));
+  char** token_backup;
+  char* token;
+
+  if (!tokens) {
+    fprintf(stderr, "jhsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, SH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += SH_TOK_BUFSIZE;
+      token_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+        free(token_backup);
+        fprintf(stderr, "jhsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+      // free(token_backup);
+    }
+
+    token = strtok(NULL, SH_TOK_DELIM);
+
+  }
+  tokens[position] = NULL;
+
+
+
+  return tokens;
+}
+
+int sh_execute(char** args) {
+  if (args[0] == NULL) {
+    return 1;
+  }
+  if (strcmp(args[0], "exit") == 0) {
+    printf("Exiting shell\n");
+    return EXIT_SUCCESS;
+  }
+
+  // for IO redirect we need to recognize "<" and ">" characters.
+
+  return sh_launch(args);
+}
+
+int sh_launch(char** args) {
+  char* temp_path = "/bin/";
+  char* full_path = NULL;
+
+  pid_t pid = fork();
+  if (pid < 0) {
+    perror("fork failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid == 0) {
+    if (args[0][0] != '/') {
+      size_t size = strlen(temp_path) + strlen(args[0]) + 1;
+      full_path = malloc(size);
+      memset(full_path, 0, size);
+      strlcpy(full_path, temp_path, size);
+      strlcat(full_path, args[0], size);
+      printf("Full path: %s\n", full_path);
+
+      (void)execv(full_path, args);
+
+      free(full_path);
+
+    } else {
+     execv(args[0], args);;
+    }
+  } else {
+    waitpid(pid, 0, 0);
+      return 1;
+  }
+  return 0;
+}
