52ef9b2189e66b2e18f22964a3795993e10abd67
johnholloway77 <jghollow@ucalgary.ca>
2025-11-09 15:46:11 -0700

    Got linked lists

--- src/single_linked_list.h
+++ src/single_linked_list.h
@@ -4,9 +4,12 @@
 
 #include <stdlib.h>
 
+#define END_OF_LIST 2
+
 #define DEFINE_SLIST(NAME, T)                                                 \
   typedef struct NAME##__node {                                               \
     struct NAME##__node* next;                                                \
+    struct NAME##__node* previous;                                            \
     T value;                                                                  \
   } NAME##__node;                                                             \
                                                                               \
@@ -20,12 +23,64 @@
     l->tail = NULL;                                                           \
   }                                                                           \
                                                                               \
+  static inline int NAME##_increment(NAME##__node** iterator, NAME* l) {      \
+    if (!l || !iterator) {                                                    \
+      return -1;                                                              \
+    }                                                                         \
+                                                                              \
+    if (!*iterator) {                                                         \
+      if (!l->head) {                                                         \
+        return -1;                                                            \
+      }                                                                       \
+      *iterator = l->head;                                                    \
+    }                                                                         \
+                                                                              \
+    if (iterator && *iterator) {                                              \
+      *iterator = (*iterator)->next;                                          \
+      if(!*iterator){ \
+        return END_OF_LIST; \
+      }\
+      return 0;                                                               \
+    }                                                                         \
+    return -1;                                                                \
+  }                                                                           \
+                                                                              \
+  static inline int NAME##_decrement(NAME##__node** iterator, NAME* l) {      \
+    if (!l || !iterator) {                                                    \
+      return -1;                                                              \
+    }                                                                         \
+                                                                              \
+    if (!*iterator) {                                                         \
+      if (!l->tail) {                                                         \
+        return -1;                                                            \
+      }                                                                       \
+      *iterator = l->tail;                                                    \
+    }                                                                         \
+                                                                              \
+    if (iterator && *iterator) {                                              \
+      *iterator = (*iterator)->previous;                                      \
+      if (!*iterator){                                                        \
+      return END_OF_LIST;                                                     \
+      }                                                                       \
+      return 0;                                                               \
+    }                                                                         \
+    return -1;                                                                \
+  }                                                                           \
+                                                                              \
   static inline int NAME##_push_front(NAME* l, T v) {                         \
+    NAME##__node* temp = l->head;                                             \
     NAME##__node* n = (NAME##__node*)malloc(sizeof *n);                       \
     if (!n) return -1;                                                        \
+                                                                              \
     n->value = v;                                                             \
     n->next = l->head;                                                        \
-    l->head = n;                                                              \
+    n->previous = NULL;                                                       \
+                                                                              \
+    if (l->head) {                                                            \
+      l->head->previous = n;                                                  \
+    } else {                                                                  \
+      l->head = n;                                                            \
+    }                                                                         \
     if (l->tail == NULL) l->tail = n; /* empty list before insert */          \
     return 0;                                                                 \
   }                                                                           \
@@ -35,6 +90,7 @@
     if (!n) return -1;                                                        \
     n->value = v;                                                             \
     n->next = NULL;                                                           \
+    n->previous = l->tail;                                                    \
     if (l->tail) {                                                            \
       l->tail->next = n;                                                      \
     } else {                                                                  \
@@ -51,12 +107,13 @@
                                                                               \
   static inline void NAME##_clear(NAME* l, void (*deleter)(T*)) {             \
     while (l->head) {                                                         \
-      NAME##__node* n =  l->head;                                            \
-      l->head = n->next;                                                    \
+      NAME##__node* n = l->head;                                              \
+      l->head = n->next;                                                      \
       if (deleter) {                                                          \
         deleter(&n->value);                                                   \
       }                                                                       \
       free(n);                                                                \
     }                                                                         \
   }
+
 #endif /* SIMPLESHELL_SINGLE_LINKED_LIST_H */
