5f3a2cfec1b29d80055f6f91b82352181047b70b
johnholloway77 <jghollow@ucalgary.ca>
2025-11-09 15:46:51 -0700

    Cleaned out unused imports and functions

--- main.c
+++ main.c
@@ -5,46 +5,10 @@
 #define _POSIX_C_SOURCE 200809L
 #define __BSD_VISIBLE 1
 
-#include <stdio.h>
-
 #include "src/sh_src.h"
-#include "src/single_linked_list.h"
 #include "src/term.h"
 
-DEFINE_SLIST(int_list, int)
-DEFINE_SLIST(str_list, char*)
-
-#define UNUSED(x) (void)(x)
-
-static void free_cstr(char** s) {
-  free(*s);
-}
-static void print_int(int* x, void *arg) {
-  UNUSED(arg);
-  printf("%d\n", *x);
-}
-static void print_str(char** s, void *arg) {
-  UNUSED(arg);
-  puts(*s);
-}
-
 int main(int argc, char** argv) {
-//  int_list A;
-//  int_list_init(&A);
-//  int_list_push_front(&A, 1);
-//  int_list_push_tail(&A, 2);
-//  int_list_push_front(&A, 0);
-//  int_list_push_tail(&A, 3);
-//  int_list_push_tail(&A, 4);
-//  int_list_foreach(&A, print_int, NULL);
-//  int_list_clear(&A, NULL);
-//
-//  str_list b;
-//  str_list_init(&b);
-//  str_list_push_tail(&b, strdup("World"));
-//  str_list_push_front(&b, strdup("Hello, "));
-//  str_list_foreach(&b, print_str, NULL);
-//  str_list_clear(&b, free_cstr);
 
   set_raw();
   atexit(restore);
