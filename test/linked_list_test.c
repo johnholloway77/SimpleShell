0259d7aa75eb521e7be88bd3a860e8bf7182129e
johnholloway77 <jghollow@ucalgary.ca>
2025-11-14 22:21:34 -0700

    Added more tests

--- test/linked_list_test.c
+++ test/linked_list_test.c
@@ -3,16 +3,53 @@
 //
 
 #include <criterion/criterion.h>
+#include <criterion/internal/assert.h>
+#include <string.h>
 
 #include "../src/macro_linked_list.h"
+#include "../src/str/strl.h"
+
+static int count = 0;
+
+void free_chars(char** c) {
+  free(*c);
+}
+
+void do_nothing(char** c) {
+  // meh
+}
+
+void count_strings(char** c, void* x) {
+  (void)(x);
+
+  if (c) {
+    count++;
+  }
+}
 
 Test(line_list_symbols, ensure_mapping_exists) {
   volatile void* p;
+
   p = (void*)line_list_init;
+  cr_expect_not_null(p);
+  (void)p;
+
   p = (void*)line_list_push_front;
+  cr_expect_not_null(p);
+  (void)p;
+
   p = (void*)line_list_push_tail;
+  cr_expect_not_null(p);
+  (void)p;
+
   p = (void*)line_list_increment;
+  cr_expect_not_null(p);
+  (void)p;
+
   p = (void*)line_list_decrement;
+  cr_expect_not_null(p);
+  (void)p;
+
   p = (void*)line_list_clear;
   (void)p;
 }
@@ -29,4 +66,150 @@ Test(line_list_init, line_list_init_use_test) {
 
   cr_expect_null(line_linked_list.head);
   cr_expect_null(line_linked_list.tail);
+}
+
+Test(line_list_init, get_value_test) {
+  line_list line_linked_list;
+
+  char* string = "This string is pushed first";
+
+  line_list_init(&line_linked_list);
+
+  cr_expect_null(line_linked_list.head);
+  cr_expect_null(line_linked_list.tail);
+
+  line_list__node* line_iterator_1 = line_linked_list.head;
+  cr_expect_null(line_iterator_1);
+
+  line_list_push_front(&line_linked_list, string);
+
+  cr_expect_not_null(line_linked_list.head);
+  cr_expect_not_null(line_linked_list.tail);
+
+  line_list__node* line_iterator = line_linked_list.head;
+
+  cr_expect_not_null(line_iterator);
+
+  char* string2 = line_list_get_value(line_iterator);
+
+  cr_expect_str_eq(string, string2);
+
+  // Can't free as it is declared on and pointing to a
+  // char* on the stack
+  // free(string2);
+}
+
+Test(line_list_init, ll_iterator_tests) {
+  char* string1 = "This string is pushed first";
+  char* string2 = "This string is pushed second";
+  char* string3 = "This string is pushed third";
+
+  line_list line_linked_list;
+  line_list_init(&line_linked_list);
+
+  line_list_push_front(&line_linked_list, string1);
+  line_list_push_tail(&line_linked_list, string2);
+  line_list_push_tail(&line_linked_list, string3);
+
+  line_list__node* line_iterator = line_linked_list.head;
+  cr_expect_not_null(line_iterator);
+
+  char* string_return_1 = line_list_get_value(line_iterator);
+  cr_expect_str_eq(string1, string_return_1);
+
+  line_list_increment(&line_iterator, &line_linked_list);
+  cr_expect_not_null(line_iterator);
+  char* string_return_2 = line_list_get_value(line_iterator);
+  cr_expect_str_eq(string2, string_return_2);
+
+  line_list_increment(&line_iterator, &line_linked_list);
+  cr_expect_not_null(line_iterator);
+  char* string_return_3 = line_list_get_value(line_iterator);
+  cr_expect_str_eq(string3, string_return_3);
+
+  line_list_increment(&line_iterator, &line_linked_list);
+  cr_expect_null(line_iterator);
+  char* string_return_x = line_list_get_value(line_iterator);
+  cr_expect_null(string_return_x);
+
+  line_list_decrement(&line_iterator, &line_linked_list);
+  cr_expect_not_null(line_iterator);
+  char* string_return_3a = line_list_get_value(line_iterator);
+  cr_expect_str_eq(string3, string_return_3a);
+
+  line_list_decrement(&line_iterator, &line_linked_list);
+  cr_expect_not_null(line_iterator);
+  char* string_return_2a = line_list_get_value(line_iterator);
+  cr_expect_str_eq(string2, string_return_2a);
+
+  line_list_decrement(&line_iterator, &line_linked_list);
+  cr_expect_not_null(line_iterator);
+  char* string_return_1a = line_list_get_value(line_iterator);
+  cr_expect_str_eq(string1, string_return_1a);
+
+  line_list_decrement(&line_iterator, &line_linked_list);
+  cr_expect_null(line_iterator);
+  char* string_return_xa = line_list_get_value(line_iterator);
+  cr_expect_null(string_return_xa);
+}
+
+Test(line_list_init, clear_all_test) {
+  size_t buffer_length = 50;
+
+  char* string1 = (char*)malloc(buffer_length);
+  char* string2 = (char*)malloc(buffer_length);
+  char* string3 = (char*)malloc(buffer_length);
+
+  char* string_a = "This string is pushed first";
+  char* string_b = "This string is pushed second";
+  char* string_c = "This string is pushed third";
+
+  strlcpy(string1, string_a, buffer_length);
+  strlcpy(string2, string_b, buffer_length);
+  strlcpy(string3, string_c, buffer_length);
+
+  cr_assert_str_eq(string_a, string1);
+  cr_assert_str_eq(string_b, string2);
+  cr_assert_str_eq(string_c, string3);
+
+  line_list line_linked_list;
+  line_list_init(&line_linked_list);
+
+  line_list_push_tail(&line_linked_list, string1);
+  line_list_push_tail(&line_linked_list, string2);
+  line_list_push_tail(&line_linked_list, string3);
+
+  line_list__node* line_iterator = line_linked_list.head;
+  cr_expect_not_null(line_iterator);
+  cr_expect_not_null(line_iterator->next);
+
+  line_list_clear(&line_linked_list, free_chars);
+  line_list__node* line_iterator2 = line_linked_list.head;
+
+  // Cannot use line as it is a use after free
+  // Undefined behavior, causes crash.
+  // cr_expect_not_null(line_iterator2->next);
+}
+
+Test(link_list_init, for_each_test) {
+  char* string1 = "This string is pushed first";
+  char* string2 = "This string is pushed second";
+  char* string3 = "This string is pushed third";
+
+  line_list line_linked_list;
+  line_list_init(&line_linked_list);
+
+  line_list_push_front(&line_linked_list, string1);
+  line_list_push_tail(&line_linked_list, string2);
+  line_list_push_tail(&line_linked_list, string3);
+
+  cr_expect_eq(count, 0);
+  line_list_foreach(&line_linked_list, count_strings, NULL);
+  cr_expect_eq(count, 3);
+
+  line_list_clear(&line_linked_list, do_nothing);
+
+  count = 0;
+  line_list_foreach(&line_linked_list, count_strings, NULL);
+  cr_expect_eq(count, 0);
 }
