f5137724835c1b47344028a3d8aedd2fe36be092
johnholloway77 <jghollow@ucalgary.ca>
2025-11-16 15:38:51 -0700

    Added signal handlers

---
+++ src/sig/sig_handlers.c
@@ -0,0 +1,37 @@
+//
+// Created by jholloway on 11/16/25.
+//
+
+#include "sig_handlers.h"
+
+#include <signal.h>
+#include <stdio.h>
+
+
+
+int init_handlers(void)
+{
+
+    struct sigaction sa;
+
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGINT, &sa, NULL) == -1){
+        (void)fprintf(stderr, "Unable t setup sighandler\n");
+        return -1;
+    }
+
+    if (sigaction(SIGQUIT, &sa, NULL) == -1){
+        (void)fprintf(stderr, "Unable t setup sighandler\n");
+        return -1;
+    }
+
+    if (sigaction(SIGTSTP, &sa, NULL) == -1){
+        (void)fprintf(stderr, "Unable t setup sighandler\n");
+        return -1;
+    }
+
+    return 0;
+}
---
+++ src/sig/sig_handlers.h
@@ -0,0 +1,10 @@
+//
+// Created by jholloway on 11/16/25.
+//
+
+#ifndef SIMPLESHELL_SIG_HANDLERS_H
+#define SIMPLESHELL_SIG_HANDLERS_H
+
+int init_handlers(void);
+
+#endif //SIMPLESHELL_SIG_HANDLERS_H
