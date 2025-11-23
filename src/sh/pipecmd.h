5c130e886f74a1f22d2768437104e833519fcf3e
johnholloway77 <jghollow@ucalgary.ca>
2025-11-23 16:55:00 -0700

    moved typedef pipecmd into separate header

---
+++ src/sh/pipecmd.h
@@ -0,0 +1,18 @@
+//
+// Created by jholloway on 11/23/25.
+//
+
+#ifndef SIMPLESHELL_PIPECMD_H
+#define SIMPLESHELL_PIPECMD_H
+
+
+typedef struct pipe_cmd {
+    int pipe_count;
+    int pipe_locations[16];  // we'll only allow a max of sixteen pipes in a
+    // command for the moment.
+    int args_length;         // number of tokens in command.
+    char** args;  // pointer to start of command (will be freed by preceding
+    // function);
+} Pipe_cmd;
+
+#endif //SIMPLESHELL_PIPECMD_H
--- src/sh/sh_src.h
+++ src/sh/sh_src.h
@@ -16,18 +16,11 @@
 
 #include "../term/toggle_echo.h"
 #include "./sh_lines.h"
+#include "pipecmd.h"
 
 #define CONT_SH_LOOP 1
 #define END_SH_LOOP 0
 
-typedef struct pipe_cmd {
-  int pipe_count;
-  int pipe_locations[16];  // we'll only allow a max of sixteen pipes in a
-                           // command for the moment.
-  int args_length;         // number of tokens in command.
-  char** args;  // pointer to start of command (will be freed by preceding
-                // function);
-} Pipe_cmd;
 
 int sh_execute(char** args, char* keep);
 void sh_init_linked_list();
