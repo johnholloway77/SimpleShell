c421b7753b3324f40c198f238c30f67198aefd05
johnholloway77 <jghollow@ucalgary.ca>
2025-11-23 17:57:38 -0700

    Moved out pipecmd typedef and removed unnecessary header includes

--- src/sh/sh_src.h
+++ src/sh/sh_src.h
@@ -5,22 +5,10 @@
 #ifndef SIMPLESHELL_SH_SRC_H
 #define SIMPLESHELL_SH_SRC_H
 
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
-#include "../term/toggle_echo.h"
-#include "./sh_lines.h"
 #include "pipecmd.h"
 
 #define CONT_SH_LOOP 1
 #define END_SH_LOOP 0
-
 
 int sh_execute(char** args, char* keep);
 void sh_init_linked_list();
