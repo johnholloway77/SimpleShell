c2a7150851ba398f0226a52ebc331aaf4098245f
johnholloway77 <jghollow@ucalgary.ca>
2025-11-23 17:57:51 -0700

    Removed as files were empty

--- src/str/update_args.c
+++
@@ -1,3 +0,0 @@
-//
-// Created by jholloway on 11/15/25.
-//
--- src/str/update_args.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by jholloway on 11/15/25.
-//
-
-#ifndef SIMPLESHELL_UPDATE_ARGS_H
-#define SIMPLESHELL_UPDATE_ARGS_H
-
-#endif  // SIMPLESHELL_UPDATE_ARGS_H
