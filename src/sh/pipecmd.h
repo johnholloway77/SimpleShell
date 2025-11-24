576227aa5d2fddf84a044698828f395407b26ae9
johnholloway77 <jghollow@ucalgary.ca>
2025-11-23 17:55:05 -0700

    formatting

--- src/sh/pipecmd.h
+++ src/sh/pipecmd.h
@@ -5,14 +5,13 @@
 #ifndef SIMPLESHELL_PIPECMD_H
 #define SIMPLESHELL_PIPECMD_H
 
-
 typedef struct pipe_cmd {
-    int pipe_count;
-    int pipe_locations[16];  // we'll only allow a max of sixteen pipes in a
-    // command for the moment.
-    int args_length;         // number of tokens in command.
-    char** args;  // pointer to start of command (will be freed by preceding
-    // function);
+  int pipe_count;
+  int pipe_locations[16];  // we'll only allow a max of sixteen pipes in a
+  // command for the moment.
+  int args_length;  // number of tokens in command.
+  char** args;      // pointer to start of command (will be freed by preceding
+                    // function);
 } Pipe_cmd;
 
-#endif //SIMPLESHELL_PIPECMD_H
+#endif  // SIMPLESHELL_PIPECMD_H
